2983aa2d832883e0742475c1b135894ff49f744d
qi weiyu <qi@weiyu.me>
2017-10-09 12:03:37 +0800

    update locales en.js

--- src/locales/en.js
+++ src/locales/en.js
@@ -2,34 +2,49 @@ export default {
   common: {
     confirm: 'Confirm',
     cancel: 'Cancel',
-    question_mark: '?',
-    create: 'Create',
-    restore: 'Restore',
-    view: 'View',
-    send: 'Send',
-  },
-  home: {
+    question_mark: '? ',
+    skynet: 'Skynet-Testnet',
+    mainnet: 'Ignition-Mainnet',
+    menu: {
+      create: 'Generate new Wallet',
+      restore_from_mnemonic: 'Restore From Mnemonic',
+      restore_from_wif: 'Restore From WIF',
+      view: 'View Wallet Info',
+      transactions: 'View Wallet Txs',
+      send: 'Send',
+      settings: 'Settings',
+    },
     info: {
       address: 'Address',
       balance: 'Balance',
       unconfirmed_balance: 'Unconfirmed Balance',
     },
+    notify: {
+      password: 'Password',
+      is_required: 'is required',
+      password_is_not_same_as_the_old_one: 'Password is not same as the old one',
+      mnemonics_are_not_same_as_the_words_should_remember: 'The mnemoincs are not same as the words you should remember',
+      mnemonics_can_not_restore: 'Those mnemonics can not restore a wallet, please check and try again',
+      address_is_not_same_as_the_old_one: 'This address is not same as the old one',
+    }
   },
   mnemonic: {
-    input_words: 'Please input the words you remembered before',
+    input_words: 'Please input words',
+    label: 'M'
   },
   password: {
-    enter: 'Please enter your Password',
+    enter: 'Please enter you password',
+    password: 'password',
   },
   send: {
     send_tokens: 'Send tokens',
-    enter_address: 'Please enter the address again (Double check)',
-    going_to_send: 'Are you going to send',
-    to_address: 'to address',
+    enter_address: 'Please enter address again (Double check)',
+    going_to_send: 'You are going to send ',
+    to_address: 'to address ',
   },
   create: {
-    title: 'Create New Wallet',
-    remember: 'Please remember the following words',
+    title: 'Generate new Wallet',
+    remember: 'Please remember the following mnemonics',
     remembered: 'I have remembered all. Let\'s check',
   },
   restore: {
@@ -37,11 +52,19 @@ export default {
   },
   view: {
     title: 'View Wallet Info',
-    recent: 'Recent 10 transactions',
-    tx: 'TX:',
+  },
+  view_tx: {
+    title: 'View Wallet Txs',
+    recent: 'Recent 10 txs',
+    tx: 'Tx: ',
     mined_at: 'Mined at ',
     total_in: 'TOTAL IN: ',
     fee: 'FEE: ',
     total_out: 'TOTAL OUT: ',
   },
+  config: {
+    title: 'Settings',
+    lan: 'Language',
+    network: 'Network',
+  }
 }
