b4bad97dd6c96a54448ec766db38bf16075cabb5
Qi Weiyu <qi@weiyu.me>
2017-10-15 22:21:18 +0800

    fix

--- src/qtum-lib.js
+++ src/qtum-lib.js
@@ -2,15 +2,23 @@ import bitcoin from 'bitcoinjs-lib'
 import config from 'config'
 
 let pubKeyHash = ''
+let wif = ''
+let scriptHash = ''
 switch (config.get('network', 'skynet'))
 {
   case 'skynet':
     pubKeyHash = 0x78
+    scriptHash = 0x6e
+    wif = 0xef
     break
   case 'mainnet':
     pubKeyHash = 0x3a
+    scriptHash = 0x32
+    wif = 0x80
     break
 }
 bitcoin.networks.bitcoin.pubKeyHash = pubKeyHash
+bitcoin.networks.bitcoin.scriptHash = scriptHash
+bitcoin.networks.bitcoin.wif = wif
 
 export default bitcoin
