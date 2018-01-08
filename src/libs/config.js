73850b49568f300bb448f38cccbc21b8b72b7bf8
Baiqiang <qiyuuu@gmail.com>
2018-01-08 13:15:08 +0800

    Change js-cookie to store.js

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,19 +1,27 @@
-import cookie from 'js-cookie'
+import store from 'store'
 import locales from 'locales'
 
 export default {
-  set(key, value) {
-    cookie.set(key, value, {expires: 1000})
+  set(key, value, expires = 86400) {
+    store.set(key, {
+      value,
+      expired_at: +new Date + expires * 1000
+    })
   },
   get(key, defaultValue) {
-    let value = cookie.get(key)
-    if (value === undefined) {
-      value = defaultValue
+    let temp = store.get(key)
+    try {
+      let value = temp.value
+      if (value === undefined || temp.expired_at < +new Date) {
+        return defaultValue
+      }
+      return value
+    } catch (e) {
+      return defaultValue
     }
-    return value
   },
   getLan() {
-    let locale = cookie.get('lan')
+    let locale = this.get('lan')
     navigator.languages.forEach(language => {
       if (locale === undefined && locales.locales.indexOf(language) !== -1) {
         locale = language
