a3781587d9e61f7833288de7e62763d7dd78d551
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2023-11-28 03:33:48 +0700

    Enable Indonesian

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,16 +1,19 @@
 import en from './en'
 import zh from './zh'
 import ko from './ko'
+import id from './id'
 
 const locales = [
   'en',
   'zh',
-  'ko'
+  'ko',
+  'id'
 ]
 const messages = {
   en,
   zh,
-  ko
+  ko,
+  id
 }
 
 export default {
